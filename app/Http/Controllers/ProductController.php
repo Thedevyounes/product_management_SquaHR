8529225a9c4d5fe862097b64916aabf8f7d515e4
Thedevyounes <bahhari.younes@gmail.com>
2023-09-01 01:06:54 +0100

    images added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::paginate(20);
+        $products = Product::paginate(10);
         return Inertia::render('Products/Index', ['products' => $products]);
     }
 
